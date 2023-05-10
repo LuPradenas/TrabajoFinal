d2e172955f751ff7503b3a1c95c7453fe24706c8
lupradenas <lucianapradenas3@gmail.com>
2023-05-10 16:45:24 -0300

    realice el carrusel de inicio y le di estilos

---
+++ Js/javascript.js
@@ -0,0 +1,10 @@
+// var carouselItems = document.querySelectorAll(".carousel-item");
+
+// // Función para cambiar el color de fondo de la diapositiva activa
+// function changeSlideBackground(color) {
+//   var activeSlide = document.querySelector(".carousel-item");
+//   activeSlide.style.backgroundColor = color;
+// }
+
+// // Ejemplo de uso: Cambiar el color de fondo de la diapositiva activa a rojo
+// changeSlideBackground("#61ada0");
